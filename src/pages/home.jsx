ea77da1ba6803b407efd57d2dbbea78b952b567c
Neha <neha27970@gmail.com>
2025-11-22 16:19:20 +0530

    setup routing and basic home component

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Topbar from '../components/Topbar'
+import Dashboard from '../components/Dashboard'
+
+const Home = () => {
+  return (
+    <>
+        <Topbar/>
+        <Dashboard/>
+    </>
+  )
+}
+
+export default Home
