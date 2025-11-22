abcf9d8fb63b76987805aa97065c7cef317abe38
Neha <neha27970@gmail.com>
2025-11-22 17:29:28 +0530

    dashboard design

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/*" element={<Home/>} />
           {/* Fallback route */}
           <Route path="*" element={<h1>Page Not Found</h1>} />
       </Routes>
